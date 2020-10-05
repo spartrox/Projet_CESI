0a11740b761c881b262550e3ca0cf1043dabc60b
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-10-05 18:40:23 +0200

    Création du fichier de configuration pour la BDD

---
+++ config/conf.php
@@ -0,0 +1,13 @@
+<?php
+class Conf
+{
+    static $debug = 1;
+    static $databases = array(
+        "default" => array(
+            "host" => "localhost",
+            "database" => "",
+            "login" => "root",
+            "password" => ""
+        )
+    );
+}
