81e5ce2b93350de69f02f52a0ae4445e3283496d
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-10-05 18:24:02 +0200

    Modification includes.php
    Nettoyage du fichier provenant d'un autre projet

--- core/includes.php
+++ core/includes.php
@@ -12,20 +12,6 @@ require 'Model.php';
 require 'Session.php';
 
 // Modèles
-require_once ROOT . DS . 'model' . DS . 'Personne.php';
-require_once ROOT . DS . 'model' . DS . 'Compte.php';
-require_once ROOT . DS . 'model' . DS . 'Arbitre.php';
-require_once ROOT . DS . 'model' . DS . 'Joueur.php';
-require_once ROOT . DS . 'model' . DS . 'Championnat.php';
-require_once ROOT . DS . 'model' . DS . 'Division.php';
-require_once ROOT . DS . 'model' . DS . 'Classement.php';
-require_once ROOT . DS . 'model' . DS . 'Equipe.php';
-require_once ROOT . DS . 'model' . DS . 'Remplacement.php';
-require_once ROOT . DS . 'model' . DS . 'Rencontre.php';
-require_once ROOT . DS . 'model' . DS . 'Journee.php';
+
 
 // Scripts
-require_once ROOT . DS . 'scripts' . DS . 'ArrayWizard.php';
-require_once ROOT . DS . 'scripts' . DS . 'IP.php';
-require_once ROOT . DS . 'scripts' . DS . 'Parser.php';
-require_once ROOT . DS . 'scripts' . DS . 'Security.php';
