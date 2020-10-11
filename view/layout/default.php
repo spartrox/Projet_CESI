e3e8ef28c748c6fef183ffa3377c32d529e87d7b
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-10-11 18:27:51 +0200

    Modification des chemins des fichiers

--- view/layout/default.php
+++ view/layout/default.php
@@ -4,21 +4,25 @@
 	    <!-- COMPATIBILITÉ -->
 	    <meta charset="utf-8"/>
 	    <meta name="viewport" content="width=device-width" />
-	    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
+		
+		<!-- Titre -->
+		<title>(Re)Sources/Lationelles</title>
 
 	      <!-- CSS/JQUERY/JAVASCRIPT -->
-	      <link rel="stylesheet" href="public/css/bootstrap.min.css">
-	      <link rel="stylesheet" type="text/css" href="public/css/style.css">
+	      <link rel="stylesheet" href="<?= BASE_SITE . DS . "/css/bootstrap.min.css" ?>">
+	      <link rel="stylesheet" type="text/css" href="<?= BASE_SITE . DS . "/css/style.css" ?>">
    		  <link href='https://fonts.googleapis.com/css?family=Raleway:100,400' rel='stylesheet' type='text/css'>
-    	  <link rel="stylesheet" href="css/font-awesome.css">
-    	  <link rel="shortcut icon" href="public/favicon/favicon.jpg">
-	      <script src="public/jquery/jquery-3.4.1.js"></script>
+		  <link rel="stylesheet" href="<?= BASE_SITE . DS . "/css/font-awesome.css" ?>">
+		  <script src="https://kit.fontawesome.com/165449b566.js" crossorigin="anonymous"></script>
+    	  <link rel="shortcut icon" href="<?= BASE_SITE . DS . "/favicon/favicon.jpg" ?>">
+	      <script src="<?= BASE_SITE . DS . "/jquery/jquery-3.4.1.js" ?>"></script>
 	      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>	
 	   
 	</head>
 	
 	<body>
-
+		<?= $content_for_layout ?>
     </body>
 </html>
