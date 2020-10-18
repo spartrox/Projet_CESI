eaca720a8989d213531c7b80dabb713296b74451
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-10-18 15:47:03 +0200

    Correction chemin Logo

--- view/layout/default.php
+++ view/layout/default.php
@@ -23,7 +23,7 @@
 	</head>
 	<div id="menu">
     	<nav class="navbar navbar-expand-lg navbar-light" id="fondMenu">
-			<a class="navbar-brand" href="index.php"><img src="<?= BASE_SITE . DS . "/images/logoMenu.png" ?>"></img></a>
+			<a class="navbar-brand" href="Accueil"><img src="<?= BASE_SITE . DS . "/images/logoMenu.png" ?>"></img></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 			</button>
