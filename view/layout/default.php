90ac35497f74717e68afb3be4c986cb8d779e5dd
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-10-18 15:06:18 +0200

    MAJ default
    - Mise en place du favicon
    - Correction du chemin vers l'accueil

--- view/layout/default.php
+++ view/layout/default.php
@@ -15,7 +15,7 @@
    		  <link href='https://fonts.googleapis.com/css?family=Raleway:100,400' rel='stylesheet' type='text/css'>
 		  <link rel="stylesheet" href="<?= BASE_SITE . DS . "/css/font-awesome.css" ?>">
 		  <script src="https://kit.fontawesome.com/165449b566.js" crossorigin="anonymous"></script>
-    	  <link rel="shortcut icon" href="<?= BASE_SITE . DS . "/favicon/favicon.jpg" ?>">
+    	  <link rel="shortcut icon" href="<?= BASE_SITE . DS . "/images/logoMenu.png" ?>">
 	      <script src="<?= BASE_SITE . DS . "/jquery/jquery-3.4.1.js" ?>"></script>
 	      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>	
@@ -32,7 +32,7 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul>
 					<li class="nav-item">
-						<a class="nav-link" href="index.php">Accueil</a>
+						<a class="nav-link" href="Accueil">Accueil</a>
 					</li>
 					<li class="nav-item">
                  	 	<a class="nav-link" href="">Connexion</a>
