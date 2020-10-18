c40d6da1b424700374f6b340793cf92a4c958423
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-10-18 15:38:46 +0200

    MAJ Accueil
    - Correction des tailles des images en responsive
    - Coorection de l'alignement des boutons de connexion et d'inscription
    - Ajout des liens vers les pages connexion/inscription

--- view/frontend/Accueil.php
+++ view/frontend/Accueil.php
@@ -3,15 +3,12 @@
 </div>
 <div class="container" style="margin-top: 2%; margin-bottom: 2%;">
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-1">
-            <button type="button">Connexion</button>
+        <div class="col-12 col-lg-6 d-flex justify-content-center">
+            <a class="btn" href="Connexion">Connexion</a>
         </div>
-        <div class="col-4"></div>
-        <div class="col-1">
-            <button type="button">Connexion</button>
+        <div class="col-12 col-lg-6 d-flex justify-content-center">
+            <a class="btn" href="Inscription">Inscription</a>
         </div>
-        <div class="col-3"></div>
     </div>
 </div>
 <div style="background-color: lightgrey; margin-bottom: 1%">
@@ -35,15 +32,15 @@
             <div><h2>Partenaires</h2></div>
         </div>
         <div class="row">
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/Cesi_Logo.png" ?>" width="300" style="display: block; margin-left: auto; margin-right: auto;"></div>
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/ministere_solidarite_sante.png" ?>" width="200" style="display: block; margin-left: auto; margin-right: auto;"></div>
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/dessin.svg.png" ?>" width="200" style="display: block; margin-left: auto; margin-right: auto;"></div>
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/La_Rochelle_logo.svg.png" ?>" width="200" style="display: block; margin-left: auto; margin-right: auto;"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/Cesi_Logo.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/ministere_solidarite_sante.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/dessin.svg.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/La_Rochelle_logo.svg.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
         </div>
         <div class="row" style="text-align:center;">
             <div class="col-3">CESI</div>
             <div class="col-3">Ministère de la solidarité et de la santé</div>
-            <div class="col-3">Confrerie 404</div>
+            <div class="col-3">La Confrerie 404</div>
             <div class="col-3">La Rochelle</div>
         </div>
     </div>
