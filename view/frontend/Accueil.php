319881c6608cfbf56cf0c109eaf11c457e526a8f
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2021-01-13 15:32:11 +0100

    Ajout des règles de styles pour l'accueil

--- view/frontend/Accueil.php
+++ view/frontend/Accueil.php
@@ -1,28 +1,28 @@
-<div style="background-color: #03989e; padding-top:5%">
-    <img src="<?= BASE_SITE . DS . "/images/logo.png" ?>" alt="Logo (Re)Sources/Lationelles" style="display: block; margin-left: auto; margin-right: auto; width: 25%">
+<div class="accueil">
+    <img src="<?= BASE_SITE . DS . "/images/logo.png" ?>" alt="Logo (Re)Sources/Lationelles" class="logo">
 </div>
-<div class="container" style="margin-top: 2%; margin-bottom: 2%;">
+<div class="container boutons">
     <div class="row">
-        <div class="col-12 col-lg-6 d-flex justify-content-center" style="">
+        <div class="col-12 col-lg-6 d-flex justify-content-center">
             <a class="btn btn-primary" href="Connexion">Connexion</a>
         </div>
-        <div class="col-12 col-lg-6 d-flex justify-content-center" style="">
+        <div class="col-12 col-lg-6 d-flex justify-content-center">
             <a class="btn btn-primary" href="Inscription">Inscription</a>
         </div>
     </div>
 </div>
-<div style="background-color: lightgrey; margin-bottom: 1%">
-    <div style="text-align: right; padding-top: 3%; padding-bottom: 3%; padding-right: 5%;">
+<div class="contour">
+    <div class="aligneDroite">
         <h3><i class="fas fa-folder-open"></i> DÉPOSER VOS RESSOURCES</h3>
     </div>
 </div>
-<div style="background-color: lightgrey; margin-bottom: 1%">
-    <div style="text-align: left;padding-top: 3%; padding-bottom: 3%; padding-left: 5%; background-color: lightgrey">
+<div class="contour">
+    <div class="aligneGauche">
         <h3>OU CRÉER VOS PROPRES SOURCES <i class="far fa-file"></i></h3>
     </div>
 </div>
-<div style="background-color: lightgrey; margin-bottom: 1%">    
-    <div style="text-align: right;padding-top: 3%; padding-bottom: 3%; padding-right: 5%; background-color: lightgrey">
+<div class="contour">    
+    <div class="aligneDroite">
         <h3><i class="far fa-user-circle"></i> COMMENTER ET PARTAGER</h3>
     </div>
 </div>
@@ -32,12 +32,12 @@
             <div><h2>Partenaires</h2></div>
         </div>
         <div class="row">
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/Cesi_Logo.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/ministere_solidarite_sante.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/dessin.svg.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
-            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/La_Rochelle_logo.svg.png" ?>" width="100%" style="display: block; margin-left: auto; margin-right: auto;"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/Cesi_Logo.png" ?>" width="100%" class="logoPartenaire"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/ministere_solidarite_sante.png" ?>" width="100%" class="logoPartenaire"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/dessin.svg.png" ?>" width="100%" class="logoPartenaire"></div>
+            <div class="col-3"><img src="<?= BASE_SITE . DS . "/images/La_Rochelle_logo.svg.png" ?>" width="100%" class="logoPartenaire"></div>
         </div>
-        <div class="row" style="text-align:center;">
+        <div class="row" class="nomPartenaire">
             <div class="col-3">CESI</div>
             <div class="col-3">Ministère des Solidarités et de la Santé</div>
             <div class="col-3">La Confrerie 404</div>
