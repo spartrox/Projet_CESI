13c1eb549c3b53e117de63fe45169036072fd551
CESIThomas <toto79230@hotmail.fr>
2021-01-13 15:18:54 +0100

    style.css

--- view/frontend/Contact.php
+++ view/frontend/Contact.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-	<title>Nous contacter</title>
-    <link rel="stylesheet" type="text/css" href="custom.css">
-    <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed" rel="stylesheet">
-</head>
-<body>
-	<div class="form ">
-		<div class="hedding-title">
-			<h1>Contactez-nous</h1>
+<div class="contact">
+	<head>
+		<title>Nous contacter</title>
+		<link rel="stylesheet" type="text/css" href="custom.css">
+		<link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed" rel="stylesheet">
+	</head>
+	<body>
+		<div class="form ">
+			<div class="hedding-title">
+				<h1>Contactez-nous</h1>
+			</div>
+			<div class="hedding-description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				quis nostrud exercitation ullamco laboris nisi ut aliquip  laborum.</p>
+			</div>
+			<div class="form-input">
+				<form>
+					Nom entier :<br>
+					<input type="text" placeholder="Nom entier...."><br>
+					Email:<br>
+					<input type="text" placeholder="Email..."></br>
+					Message:<br>
+					<textarea rows="4" placeholder="Message.."></textarea>
+					<button class="button">Envoyer</button>
+				</form> 
+			</div>
 		</div>
-		<div class="hedding-description">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			quis nostrud exercitation ullamco laboris nisi ut aliquip  laborum.</p>
-		</div>
-		<div class="form-input">
-			<form>
-				Nom entier :<br>
-				<input type="text" placeholder="Nom entier...."><br>
-				Email:<br>
-				<input type="text" placeholder="Email..."></br>
-				Message:<br>
-				<textarea rows="4" placeholder="Message.."></textarea>
-				<button class="button">Envoyer</button>
-			</form> 
-		</div>
-	</div>
-</body>
+	</body>
+</div>
 </html>
 
 <!-- CODE CSS CONTACT
