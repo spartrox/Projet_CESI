3058f13f849a5e2752c2c1665dc4e3801ca786d8
CESIThomas <toto79230@hotmail.fr>
2021-01-13 15:25:34 +0100

    Update Contact.php

--- view/frontend/Contact.php
+++ view/frontend/Contact.php
@@ -9,7 +9,7 @@
 	<body>
 		<div class="form ">
 			<div class="hedding-title">
-				<h1>Contactez-nous</h1>
+				<h1>Contactez-nous</h1><br>
 			</div>
 			<div class="hedding-description">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -22,7 +22,7 @@
 					Email:<br>
 					<input type="text" placeholder="Email..."></br>
 					Message:<br>
-					<textarea rows="4" placeholder="Message.."></textarea>
+					<textarea rows="4" placeholder="Message.."></textarea><br>
 					<button class="button">Envoyer</button>
 				</form> 
 			</div>
@@ -30,59 +30,3 @@
 	</body>
 </div>
 </html>
-
-<!-- CODE CSS CONTACT
-body{
-	background-color:#c1c1c1;
-	font-family: 'Barlow Semi Condensed', sans-serif;
-}
-.form{
-	padding:30px;
-	width:50%;
-	margin:0 auto;
-	color:#fff;
-	margin-top:30px;
-	border:1px solid #FF9656;
-	border-radius: 5px;
-	background-color: #333;
-}
-.hedding-title{
-	font-size:25px;
-	text-align: center;
-}
-.hedding-title h1{
-	margin:0px;
-	color:#ff9656;
-	font-weight: normal;
-}
-.hedding-description{
-	text-align: center;
-}
-.form-input input,
-.form-input textarea{
-	width:100%;
-	margin: 15px 0px;
-	font-size:18px;
-	font-family:"Barlow Semi Condensed",sans-serif;
-	border-radius: 0px;
-	border: 1px solid #c1c1c1;
-}
-.button{
-	padding:8px 0px;
-	width: 100%;
-	margin-top:15px;
-	background-color:#FF9656;
-	border: none;
-	color: #fff;
-}
-@media (min-width:320px) and (max-width:640px){
-	.form{
-		width:80%;
-	}
-}
-@media (min-width:768px) and (max-width:1024px){
-	.form{
-		width:60%;
-	}
-}
--->
