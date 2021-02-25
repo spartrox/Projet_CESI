e9b8160f57c219ec23f98f544b1fbd6ce66c504c
CESIThomas <toto79230@hotmail.fr>
2021-02-25 15:09:54 +0100

    Update Contact.php

--- view/frontend/Contact.php
+++ view/frontend/Contact.php
@@ -1,35 +1,64 @@
-<!DOCTYPE html>
-<html lang="fr">
-<div class="contact">
-	<head>
-		<title>Nous contacter</title>
-		<link rel="stylesheet" type="text/css" href="custom.css">
-		<link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed" rel="stylesheet">
-	</head>
-	<body>
-		<div class="form ">
-			<div class="hedding-title">
-				<h1>Contactez-nous</h1><br>
-			</div>
-			<div class="hedding-description">
-				<p>Pour nous contacter directement par mail il vous suffit de compléter le formulaire de contact
-				ci-dessous en renseignant les champs demandés.</p>
-			</div>
-			<div class="form-input">
-				<form>
-					Nom entier :<br>
-					<input type="text" placeholder="Nom entier...."><br>
-					<br>
-					Email:<br>
-					<input type="text" placeholder="Email..."></br>
-					<br>
-					Message:<br>
-					<textarea rows="4" placeholder="Message.."></textarea><br>
-					<br>
-					<button class="button">Envoyer</button>
-				</form> 
-			</div>
-		</div>
-	</body>
-</div>
-</html>
+<?php
+/* Page: contact.php */
+$VotreAdresseMail="votreemail@votresite.tld";//mettez ici votre adresse mail
+if(isset($_POST['envoyer'])) { // si le bouton "Envoyer" est appuyé
+    //on vérifie que le champ mail est correctement rempli
+    if(empty($_POST['mail'])) {
+        echo "Le champ mail est vide";
+    } else {
+        //on vérifie que l'adresse est correcte
+        if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i",$_POST['mail'])){
+            echo "L'adresse mail entrée est incorrecte";
+        }else{
+            //on vérifie que le champ sujet est correctement rempli
+            if(empty($_POST['sujet'])) {
+                echo "Le champ sujet est vide";
+            }else{
+                //on vérifie que le champ message n'est pas vide
+                if(empty($_POST['message'])) {
+                    echo "Le champ message est vide";
+                }else{
+                    //tout est correctement renseigné, on envoi le mail
+                    //on renseigne les entêtes de la fonction mail de PHP
+                    $Entetes = "MIME-Version: 1.0\r\n";
+                    $Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
+                    $Entetes .= "From: Nom de votre site <".$_POST['mail'].">\r\n";//de préférence une adresse avec le même domaine de là où, vous utilisez ce code, cela permet un envoie quasi certain jusqu'au destinataire
+                    $Entetes .= "Reply-To: Nom de votre site <".$_POST['mail'].">\r\n";
+                    //on prépare les champs:
+                    $Mail=$_POST['mail']; 
+                    $Sujet='=?UTF-8?B?'.base64_encode($_POST['sujet']).'?=';//Cet encodage (base64_encode) est fait pour permettre aux informations binaires d'être manipulées par les systèmes qui ne gèrent pas correctement les 8 bits (=?UTF-8?B? est une norme afin de transmettre correctement les caractères de la chaine)
+                    $Message=htmlentities($_POST['message'],ENT_QUOTES,"UTF-8");//htmlentities() converti tous les accents en entités HTML, ENT_QUOTES Convertit en + les guillemets doubles et les guillemets simples, en entités HTML
+                    //en fin, on envoi le mail
+                    if(mail($VotreAdresseMail,$Sujet,nl2br($Message),$Entetes)){//la fonction nl2br permet de conserver les sauts de ligne et la fonction base64_encode de conserver les accents dans le titre
+                        echo "Le mail à été envoyé avec succès!";
+                    } else {
+                        echo "Une erreur est survenue, le mail n'a pas été envoyé";
+                    }
+                }
+            }
+        }
+    }
+}
+?>
+
+<!--
+Les balises <form> sert à dire que c'est un formulaire
+on lui demande de faire fonctionner la page contact.php
+une fois le bouton "Envoyer" cliqué on lui dit également que c'est un formulaire de type "POST"
+Les balises <input> sont les champs de formulaire
+type="text" sera du texte
+la balise <textarea> sert à dire qu'il faut afficher un champ de texte rectangulaire (les sauts de ligne sont possibles) au contraire des champ input text
+Vous remarquerez que l'ont ne renseigne pas le type (type="") pour la balise textarea
+cols="nombre de colone horizontale" rows="nombre de colone verticales"
+type="submit" sera un bouton pour valider le formulaire
+name="nom de l'input" sert à le reconnaitre une fois le bouton submit cliqué, pour le code PHP contact.php
+-->
+<form action="contact.php" method="post">
+    Mail: <input type="text" name="mail" value="" />
+    <br />
+    Sujet: <input type="text" name="sujet" value="" />
+    <br />
+    Message: <textarea name="message" cols="40" rows="20"></textarea>
+    <br />
+    <input type="submit" name="envoyer" value="Envoyer" />
+</form>
